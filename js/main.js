76b55ce05e127916978998c659c25d76a09abd0e
DV3 <mahmoudelkhateb18@gmail.com>
2021-06-23 14:25:05 +0200

    all

--- js/main.js
+++ js/main.js
@@ -26,3 +26,106 @@ $(document).ready(function() {
         }
     });
 });
+// attach every nav link with their corresponding section 
+$(document).ready(function() {
+    $(".navbar-nav .nav-item .nav-link").click(function() {
+        $(this).parent().addClass("active").siblings().removeClass("active");
+        $("body, html").animate({
+                // scrollTop = divId.offset().top
+                scrollTop: $($(this).data("target")).offset().top - $('.navbar').innerHeight()
+            },
+            500
+        );
+    });
+    $(".navbar-nav .nav-item .nav-link").click(function() {
+        $(".navbar-collapse").removeClass("show");
+    })
+    $(window).scroll(function () {
+    //async active  nav links with sections
+    $("section.async").each(function () {
+        if ($(window).scrollTop() >= $(this).offset().top - $(".navbar").innerHeight()) {
+          var bebo = $(this).attr("id");
+          $(".navbar-nav .nav-item").removeClass("active");
+          $('.navbar-nav .nav-item a[data-slide = "' + bebo + '"]')
+            .parent()
+            .addClass("active");
+        }
+      });
+    })
+
+
+
+    // adjust left part depend on sideBar width 
+
+    
+    $('.brand-toggle').click(function(){
+      var mySide = $('.sid-bar').innerWidth();
+      $('.left-part').css({
+        marginRight : mySide
+     })
+
+    })
+
+    $('.left-part').css({
+      marginRight : $('.sid-bar').innerWidth()
+   })
+})
+
+
+
+
+
+
+
+
+// charts 
+
+//line
+var ctxL = document.getElementById("lineChart1").getContext('2d');
+var myLineChart = new Chart(ctxL, {
+  type: 'line',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October"],
+    datasets: [{
+      pointBackgroundColor: '#fff',
+      backgroundColor: 'transparent',
+      borderColor: 'rgba(255, 255, 255)',
+      data: [2500, 2550, 5000, 3100, 7000, 5500, 4950, 16000, 10500, 8000],
+    }]
+  },
+  options: {
+    legend: {
+      display: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+          color: "transparent",
+          zeroLineColor: "transparent"
+        },
+        ticks: {
+          fontColor: "#fff",
+        },
+      }],
+      yAxes: [{
+        display: true,
+        gridLines: {
+          display: true,
+          drawBorder: false,
+          color: "rgba(255,255,255,.25)",
+          zeroLineColor: "rgba(255,255,255,.25)"
+        },
+        ticks: {
+          fontColor: "#fff",
+          beginAtZero: true,
+          stepSize: 5000
+        },
+      }],
+    }
+  }
+});
+
+
+
+
